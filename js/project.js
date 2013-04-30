477c793bb5443db5e280315dc48af1e26640d3a2
slevin <slevin@mailer.ag>
2013-04-30 18:46:06 +0300

    v.1.1.0 added 'Servers' tab to know on what server each site is placed

--- js/project.js
+++ js/project.js
@@ -1,24 +1,25 @@
+// workaround for local tests
+if (chrome.runtime === undefined) {
+	chrome.runtime = {getManifest : function () { return {version : 'local-dev'}; }};
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	function unifyStorageKey(key) {
-		return 'pkw-selector.' + key;
+		return "pkw-selector." + key;
 	}
 
-	var extensionVersion = chrome.runtime.getManifest().version;
-	$('.crx-version').text(extensionVersion);
+	$(".crx-version").text(chrome.runtime.getManifest().version);
 	// https://developer.chrome.com/extensions/storage.html#method-StorageArea-get
 	// http://developer.chrome.com/extensions/manifest.html
 	// http://developer.chrome.com/extensions/manifestVersion.html
 	// http://stackoverflow.com/questions/7573191/can-i-access-a-property-from-my-manifest-json-in-my-extensions-javascript-files
 
-	$('.loaddata').on('click', '');
-
-	$.ajax({
-		url: "http://test.piecesauto24.com/crx-selector-api/index.php?/listSites",
-		success: function(data){
-			$(".loaddata").html(data);
+	$.get(
+		"http://test.piecesauto24.com/crx-selector-api/index.php?/listSites",
+		{},
+		function (sitesHtml) {
+			$(".sites-list").html(sitesHtml);
 			var selectedSkin = localStorage.getItem(unifyStorageKey('skin'));
-
-			console.log(unifyStorageKey('skin'), selectedSkin);
 
 			if (selectedSkin !== null) {
 				$("#sites li a[data-skin=\"" + selectedSkin + "\"]").click();
@@ -28,9 +29,17 @@ document.addEventListener("DOMContentLoaded", function () {
 				e.preventDefault();
 				chrome.tabs.create({url : $(this).attr('href')});
 			});
-			$("#sites li a").on("click", function() {
-				localStorage.setItem(unifyStorageKey('skin'), $(this).data('skin'));
+			$("#sites li a").on("click", function () {
+				localStorage.setItem(unifyStorageKey("skin"), $(this).data("skin"));
 			});
 		}
-	});
+	);
+
+	$.get(
+		"http://test.piecesauto24.com/crx-selector-api/index.php?/listServers",
+		{},
+		function (serversHtml) {
+			$(".server-deployment").html(serversHtml);
+		}
+	);
 });
